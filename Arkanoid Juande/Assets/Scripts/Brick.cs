dba0631681cfe064611209770f04d777a09b4166
JuanDeDiosGarcia <61006172+JuanDeDiosGarcia@users.noreply.github.com>
2024-10-25 11:24:11 +0200

    Bloques
    Creados varios tipos de bloques.
    Creado sistema de particulas al romper bloques.

--- Arkanoid Juande/Assets/Scripts/Brick.cs
+++ Arkanoid Juande/Assets/Scripts/Brick.cs
@@ -1,21 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-
+    public int lives = 1; // Vidas del brick
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            Destroy(gameObject);
+            LoseLife();
         }
     }
 
+    private void LoseLife()
+    {
+        lives--; // Reduce una vida
 
+        if (lives <= 0)
+        {
+            // Obtén la referencia al ParticleManager y crea el efecto
+            ParticleManager particleManager = FindObjectOfType<ParticleManager>();
+            if (particleManager != null)
+            {
+                particleManager.CreateBrickDeathParticle(transform.position);
+            }
 
-
-
+            Destroy(gameObject); // Destruye el brick si no tiene más vidas
+        }
+    }
 }
---
+++ Arkanoid Juande/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public GameObject particlePrefab; // Prefab de partículas
+
+    public void CreateBrickDeathParticle(Vector3 position)
+    {
+        Instantiate(particlePrefab, position, Quaternion.identity);
+    }
+}
